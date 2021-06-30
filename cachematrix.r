bae84ae00f3be92dff07c4d7dab258b8780ffb27
barjean <86595675+barjean@users.noreply.github.com>
2021-06-30 01:13:55 -0400

    Create cachematrix.r

---
+++ cachematrix.r
@@ -0,0 +1,29 @@
+## script is storing the inverse of the matrix in the parent environment
+## defining j, x, y, and set in the global environment
+makeCacheMatrix <- function(x = matrix()) {o
+  j <- NULL
+  set <- function(y){
+  x <<- y
+  j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+  setInverse = setInverse, 
+  getInverse = getInverse)
+}
+
+## script is calling the inverse matrix
+
+cacheSolve <- function(x, ...) {
+## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+  message("getting cached data")
+  return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
